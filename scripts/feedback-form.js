95214e49fa73321c6f301cac00d492fa1db07423
John Pier <warhamer10tw@gmail.com>
2020-11-27 01:37:29 +0400

    update

--- scripts/feedback-form.js
+++ scripts/feedback-form.js
@@ -1,31 +1,59 @@
+import { openPopover } from "./popover.js";
+
 let form;
 
 const feedbackFormName = "feedback-form";
+
 const feedbackFormFieldNames = {
     EMAIL: "email",
     NAME: "name",
     TEXT_AREA: "text",
     COURSE: "course",
-    TOOLBAR: "toolbar",
+    MAIN_FIELDS: "main-fields",
     SUBMIT: "submit",
-    CHECKBOX: "requireAnswer"
+    CHECKBOX: "require-answer"
 };
 
 document.addEventListener("DOMContentLoaded", () => {
     form = document.forms[feedbackFormName];
-    console.log(form, form.elements);
 
     form.onsubmit = (event) => {
-        console.log(event);
         event.preventDefault();
+        event.stopPropagation();
+        openPopover(getValuesAsElement(), () => clearForm());
     };
 
-    Object.values(feedbackFormFieldNames).forEach(value => {
-        form.elements[value].addEventListener("change", event => {
-            console.log(event);
-        });
-    })
-});
+    form.elements[feedbackFormFieldNames.CHECKBOX].addEventListener("change", event => {
+        if (event.target.checked) {
+            form.elements[feedbackFormFieldNames.EMAIL].setAttribute("required", "true");
+        } else {
+            form.elements[feedbackFormFieldNames.EMAIL].removeAttribute("required");
+        }
 
+    });
+
+    function getValuesAsElement() {
+        const element = document.createElement("div");
+        element.className = "popover__list";
+        element.innerHTML = element.innerHTML.concat(
+            "<h3>Введенные данные:</h3>",
+            getStroke("Email", form.elements[feedbackFormFieldNames.EMAIL].value),
+            getStroke("Имя", form.elements[feedbackFormFieldNames.NAME].value),
+            getStroke("Сообщение", form.elements[feedbackFormFieldNames.TEXT_AREA].value),
+            getStroke("Курс", form.elements[feedbackFormFieldNames.COURSE].value),
+            getStroke("Необходимо ответить", form.elements[feedbackFormFieldNames.CHECKBOX].checked),
+        );
+
+        function getStroke(name, value) {
+            return ("<div>" + name + ": " + (value || "-") + "</div>");
+        }
+
+        return element;
+    }
+
+    function clearForm() {
+        form.reset();
+    }
+});
 
 
